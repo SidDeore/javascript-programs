532e54368eb7c52d0e32cf294a396438b832e6ea
SidDeore <113504059+SidDeore@users.noreply.github.com>
2024-07-31 14:32:30 +0530

    Assignments-On-Variables-and-Datatypes
    Assignments-On-Variables-and-Datatypes

---
+++ 01-variableAssign.js
@@ -0,0 +1,30 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Siddheshwar Deore";
+console.log("My Name is:",myFullName);
+myFullName="Siddheshwar Dnyaneshwar Deore";
+console.log("My FullName is:",myFullName);
+
+var mylove="I love only JS";
+console.log(mylove);
+
+var firstName="Siddheshwar";
+console.log("My First Name is:",firstName);
+
+var lastName="Deore";
+console.log("My Last Name is:",lastName);
+
+var age="23";
+console.log("My Age is:",age);
+
+var address="Karvenagar, Pune";
+console.log("My Address is:",address);
+
+var pinCode="411052";
+console.log("Pincode is:",pinCode);
+
+var gender="Male";
+console.log("Gender",gender);
+
+var isMarried="No";
+console.log("isMarried",isMarried);
---
+++ 02-DatatypeAssign.js
@@ -0,0 +1,29 @@
+console.log("======================BankName======================");
+var bankname;
+console.log(bankname);
+console.log("DataType Of BankName is:",typeof bankname);
+var bankname="SBI Bank";
+console.log("Bank Name is:",bankname);
+console.log("DataType Of BankName is:",typeof bankname);
+
+console.log("======================Marks==========================");
+var marks="90%";
+console.log("Mark is:",marks);
+console.log("Datatype of marks is:", typeof marks);
+
+console.log("======================isWorking======================");
+var isWorking;
+console.log(isWorking);
+console.log("Datatype of isWorking is:", typeof isWorking);
+isWorking= true;
+console.log("isWorking:",isWorking);
+console.log("Datatype of isWorking is:", typeof isWorking);
+
+console.log("======================TotalCount======================");
+var totalCount="Hundred and Seven";
+console.log("totalCount:",totalCount);
+console.log("Datatype of totalCount:",typeof totalCount);
+totalCount=107;
+console.log("totalCount:",totalCount);
+console.log("Datatype of totalCount:",typeof totalCount);
+
