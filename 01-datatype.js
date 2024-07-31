64bb648431757aee022acf0b3c9d270795e8820a
SidDeore <113504059+SidDeore@users.noreply.github.com>
2024-07-31 14:42:44 +0530

    datatype-practice
    datatype-practice

---
+++ 01-datatype.js
@@ -0,0 +1,40 @@
+// Primitive Datatype: Number, String and Boolean
+console.log("============= Number Datatype ===============");
+var age = 22;
+console.log("Age is:", age);
+var ageTypeOf = typeof age;
+console.log("Datatype of age is: ", ageTypeOf);
+
+console.log("============= String Datatype ===============");
+var city = 'Pune';
+console.log("City is:", city);
+var cityTypeOf = typeof city;
+console.log("DataType of city is:", cityTypeOf);
+
+var marks = "93";
+console.log("Mark is: ", marks);
+var marksTypeOf = typeof marks;
+console.log("DataType of marks is:", marksTypeOf);
+
+console.log("============= Boolean Datatype ===============");
+var isMarried = true;
+console.log("isMarried : ", isMarried);
+var isMarriedTypeOf = typeof isMarried;
+console.log("isMarried Datatype of is: ", isMarriedTypeOf);
+
+var isFailed = false;
+console.log("isFailed : ", isFailed);
+var isFailedTypeOf = typeof isFailed;
+console.log("isFailed Datatype of is: ", isFailedTypeOf);
+
+console.log("============= Special Datatype: undefined ===============");
+var country;
+console.log("Country value is: ", country);
+var countryTypeOf = typeof country;
+console.log("DataType of country is: ", countryTypeOf);
+
+console.log("============= Special Datatype: null ===============");
+var village = null;
+console.log("Value of village is: ", village);
+var villageTypeOf = typeof village;
+console.log("DataType of village:", villageTypeOf);
