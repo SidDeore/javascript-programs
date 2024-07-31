f8a72f885fa78ffc549f63bb3f2f736ca4ea1d31
SidDeore <113504059+SidDeore@users.noreply.github.com>
2024-07-31 07:46:49 +0530

    JavaScript-01-Assignment-Variables
    JavaScript-01-Assignment-Variables

---
+++ 01-VariableAssignment.js
@@ -0,0 +1,5 @@
+console.log("My city name is= Chhatrapati Sambhajinagar");
+console.log("My best friend name is= Jimmy");
+console.log("My country name is= India");
+console.log("My school name is= Maharashtra Public School, Chh. Sambhajinagar");
+console.log("My college name is= G H Raisoni Institute of Engg & Tech, Pune");
