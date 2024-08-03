17aa9c6dd3a36efc6b891b323e6861ad118d9f25
SidDeore <113504059+SidDeore@users.noreply.github.com>
2024-08-03 16:29:32 +0530

    03-stringBasicAssign
    string Basics Assignment practice

---
+++ 03-stringBasicAssign01.js
@@ -0,0 +1,15 @@
+function stringBasic(){
+    console.log("========= Dream Job =========");
+    console.log(`My dream company is:"Amdocs"`);
+    console.log("========= Hobbies ===========");
+    var hobby1=" Volleyball";
+    var hobby2=" Travel";
+    var hobby3=" Coding";
+    console.log(`My Hobbies are:${hobby1},${hobby2},${hobby3}`);
+    var char1=hobby1.length;
+    var char2=hobby2.length;
+    var char3=hobby2.length;
+    console.log("====== Length of string and sum od string length ======");
+    console.log("total number of characters are:",char1+char2+char3);    
+}
+stringBasic();
