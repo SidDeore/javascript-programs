5171b8a727b00770844873203e945033a2e0555d
SidDeore <113504059+SidDeore@users.noreply.github.com>
2024-08-15 22:51:34 +0530

    08-arrayAssign-Remaind-Steps
    Remaining steps arrayNum

--- 08-arrayAssign02.js
+++ 08-arrayAssign02.js
@@ -2,13 +2,16 @@ const array = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
 console.log(`======== Total element of array =========`);
 console.log(`Given Array: ${array}`);
 console.log(`Total Element of Array: ${array.length}`);
+
 console.log(`======== First and Last Element of Array =========`);
 const arrayNum = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
 // console.log(`Given Array: ${array}`);
 console.log(`1st element: ${arrayNum[0]} and Last Element: ${arrayNum[arrayNum.length-1]}`);
+
 console.log(`======== Third Last element of Array =========`);
 const arrayNum1 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
 console.log(`Third Last Element: ${arrayNum1[arrayNum1.length-3]}`);
+
 console.log(`======== All Even no of Array =========`);
 const arrayNum2 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
 let evenNumber=[];
@@ -18,6 +21,7 @@ for (const element of arrayNum2) {
     }
 }
 console.log(`All Even no of Array: ${evenNumber}`);
+
 console.log(`======== All Odd no of Array =========`);
 const arrayNum3 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
 // console.log(`Given Array: ${array}`);
@@ -28,6 +32,39 @@ for (const element of arrayNum3) {
     }
 }
 console.log(`All Odd no of Array: ${oddNumber}`);
+
+console.log(`======== Sum of all Even positioned elements =========`);
+const arrayNum10 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
+let sumevenpos = 0;
+for (let index = 0; index < arrayNum10.length; index++) {
+    if (index%2==0) {
+        const element = arrayNum10[index]
+        sumevenpos = sumevenpos+element;
+    }
+    
+}
+console.log(`Sum of all even positioned elements are: ${sumevenpos}`);
+
+console.log(`======== Sum of all Odd positioned elements =========`);
+const arrayNum11 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
+let sumoddpos = 0;
+for (let index = 0; index < arrayNum11.length; index++) {
+    if (index%2==1) {
+        const element = arrayNum11[index]
+        sumoddpos = sumoddpos+element;
+    }
+    
+}
+console.log(`Sum of all Odd positioned elements are: ${sumoddpos}`);
+
+console.log(`======== Sum of all elements =========`);
+const arrayNum9 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
+let sum = 0;
+for (const element of arrayNum9) {
+        sum=sum+element;
+}
+console.log(`Sum of all elements are: ${sum}`);
+
 console.log(`======== Numbers which are multiple of 5 =========`);
 const arrayNum4 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
 let multiple=[]
