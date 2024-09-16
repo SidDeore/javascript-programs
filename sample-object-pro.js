602a1a045956968eb7f542cefcc2537dd208e9bc
SidDeore <113504059+SidDeore@users.noreply.github.com>
2024-09-17 03:21:27 +0530

    sample-object-pro-Chnages-6to10
    New-program-6to10

--- sample-object-pro.js
+++ sample-object-pro.js
@@ -6,7 +6,6 @@ var student = {
 }
 let properties = Object.keys(student)
 console.log(properties);
-console.table(student);
 
 console.log("========= pro 02: Delete one property of properties of object ===========");
 var student = {
@@ -57,3 +56,64 @@ function modifySaraly(id, newSalary) {
 }
 modifySaraly(1, 60000);
 console.table(employee);
+
+console.log("========= pro 06. How to get the last item of JavaScript object ===========");
+console.log('i). Using Object.keys() Method');
+var car = {
+    make : "Toyota",
+    model : "Carmy",
+    year : 2020,
+    color : "blue"
+}
+let keys = Object.keys(car);
+let lastKey = keys[keys.length-1];
+console.log(`Last item of object= '${lastKey} : ${car[lastKey]}'`);
+
+console.log('ii). Using Object.entries() Method');
+var car = {
+    make : "Toyota",
+    model : "Carmy",
+    year : 2020,
+    color : "blue"
+}
+let entries = Object.entries(car);
+let lastEntry = entries[entries.length-1];
+console.log(`Last Item of Onject = '${lastEntry[0]} : ${lastEntry[1]}'`);
+
+console.log("========= pro 08. How to add an object to an array in JavaScript ===========");
+var fruits = ['apple', 'banana', 'orange'];
+var newFruit = {
+    name : 'grape',
+    color : 'purple'
+};
+fruits.push(newFruit);
+console.log('Add an object to array', ...fruits);
+
+console.log("========= pro 09. How to remove duplicates from an array of objects using JavaScript ===========");
+var student = [
+    {id: 1, name: 'Sid'},
+    {id: 2, name: 'vishal'},
+    {id: 1, name: 'Sid'},
+    {id: 3, name: 'Adi'},
+    {id: 2, name: 'vishal'}
+]
+function removeArrayObjectDuplicate(arr) {
+    return arr.filter((student, index, self)=>
+        index == self.findIndex((t)=>(
+        t.id == student.id && t.name == student.name
+        ))
+    );
+}
+var result = removeArrayObjectDuplicate(student);
+console.table(result);
+
+console.log("========= pro 10. How to get a subset of a javascript object’s properties? ===========");
+var person = {
+    firstName : 'Sid',
+    lastName : 'Deore',
+    age : 23,
+    city : 'Pune',
+    country : 'India'
+}
+var {firstName, lastName} = person;
+console.log(firstName, lastName);
